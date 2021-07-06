a83ed75c6e8e3e513d3a976ad79f65b2497d4eb9
Simona Simonovska <simona.simonovska@atis.al>
2021-07-06 17:25:00 +0200

    dashboard style

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -5,28 +5,14 @@ interface Props {}
 
 export const Board = (props: Props) => {
   return (
-    <div className="board">
-      <div className="oponent-section">
-        <Card
-          cardColor=""
-          cardNumber=""
-          cardSign=""
-          front={false}
-        />
-          <Card
-          cardColor=""
-          cardNumber=""
-          cardSign=""
-          front={false}
-        />
-          <Card
-          cardColor=""
-          cardNumber=""
-          cardSign=""
-          front={false}
-        />
-      </div>
-      <div className="middleBoard">
+    <>
+      <div className="board">
+        <div className="oponent-section">
+          <Card cardColor="" cardNumber="" cardSign="" front={false} />
+          <Card cardColor="" cardNumber="" cardSign="" front={false} />
+          <Card cardColor="" cardNumber="" cardSign="" front={false} />
+        </div>
+        <div className="middleBoard">
           <div className="currentCard">
             <Card
               cardColor="black"
@@ -43,26 +29,46 @@ export const Board = (props: Props) => {
               front={false}
             />
           </div>
-      </div>
-      <div className="player-section">
-        <Card
-          cardColor=""
-          cardNumber=""
-          cardSign=""
-          front={true}
-          jokerCard={true}
-        />
-           <Card
-          cardColor="red"
-          cardNumber="6"
-          cardSign="&hearts;"
-          front={true}
-        />
+        </div>
+        <div className="player-section">
+          <Card
+            cardColor=""
+            cardNumber=""
+            cardSign=""
+            front={true}
+            jokerCard={true}
+          />
+          <Card
+            cardColor="red"
+            cardNumber="6"
+            cardSign="&hearts;"
+            front={true}
+          />
+        </div>
       </div>
       <div className="dashboard">
+        <div className="gameNumber">Game #3</div>
+        <div className="playersLabel">Players: </div>
+        <div className="playersSection">
+          <div className="names">
+            <div className="playerOne">Simon</div>
+            <div className="playerTwo">Simona</div>
+          </div>
+          <div className="wins">
+            <div className="winOne"><span>1</span></div>
+            <div className="winTwo"><span>2</span></div>
+          </div>
+        </div>
+        <div className="winnerLabel">
+        WINNER!
+        </div>
+        <div className="winnerName">
+          Simona
+        </div>
+      
+          <img className="icon" src={require('../assets/celebrate.png').default} alt="" />
         
       </div>
-    </div>
-
+    </>
   );
 };
